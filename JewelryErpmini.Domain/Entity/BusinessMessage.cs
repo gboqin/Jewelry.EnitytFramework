c711390c273c5dfcc79df3f233f1c0499321c1f2
gboqin <gb.qin@163.com>
2015-11-23 14:20:46 +0800

    Jewelry20151123003

---
+++ JewelryErpmini.Domain/Entity/BusinessMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelryErpmini.Domain.Entity
+{
+    [Table("view_businessOrdersProgressMessage")]
+    public class BusinessMessage
+    {
+        [Required]
+        public int countTypeId { get; set; }
+        [StringLength(16)]
+        [Required]
+        public string countType { get; set; }
+        public int? total { get; set; }
+    }
+}
--- JewelryErpmini.Infrastructure/JewelryDbContext.cs
+++ JewelryErpmini.Infrastructure/JewelryDbContext.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using JewelryErpmini.Domain.Entity;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,15 @@ namespace JewelryErpmini.Infrastructure
 {
     public class JewelryDbContext:DbContext
     {
+        #region 构造函数
+        public JewelryDbContext() : base("default") { }
+        public JewelryDbContext(string NameOrConnectionString) : base(NameOrConnectionString) { }
+        #endregion
+        public IDbSet<BusinessMessage> BusinessMessages { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
